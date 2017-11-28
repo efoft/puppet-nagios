edc9908f5ebd4be4b0a7683d5b3c8b92e7be096c
pajan la <pajan@yandex.ru>
2017-11-28 19:15:51 +0300

    servers param can have single value or array

--- manifests/client.pp
+++ manifests/client.pp
@@ -4,6 +4,8 @@
 # === Parameters ===
 # [*servers*]
 #   From where nrpe allows connections from.
+#   Can be single value or and array.
+#   Default: 127.0.0.1
 #
 # [*myip*]
 #   IP reachable from nagios server.
@@ -43,63 +45,63 @@
 # All the rest parameters are swithes to specific checks and some params for them (like passwords).
 #
 class nagios::client(
-  Enum['present','absent'] $ensure    = 'present',
-  Array[String] $servers              = ['127.0.0.1'],
-  Stdlib::Compat::Ipv4 $myip          = $::ipaddress,
-  Hash $contacts                      = {},
-  Numeric $nrpe_listen_port           = $nagios::params::nrpe_listen_port,
-  Optional[String] $nrpe_bind_address = $nagios::params::nrpe_bind_address,
-  Boolean $nrpe_allow_args            = $nagios::params::nrpe_allow_args,
-  Boolean $nrpe_debug                 = $nagios::params::nrpe_debug,
-  Boolean $defaultchecks              = true,
+  Enum['present','absent'] $ensure       = 'present',
+  Variant[Array[String],String] $servers = '127.0.0.1',
+  Stdlib::Compat::Ipv4 $myip             = $::ipaddress,
+  Hash $contacts                         = {},
+  Numeric $nrpe_listen_port              = $nagios::params::nrpe_listen_port,
+  Optional[String] $nrpe_bind_address    = $nagios::params::nrpe_bind_address,
+  Boolean $nrpe_allow_args               = $nagios::params::nrpe_allow_args,
+  Boolean $nrpe_debug                    = $nagios::params::nrpe_debug,
+  Boolean $defaultchecks                 = true,
   # below are defaultchecks, can be disabled here individually
   ## linux
-  Boolean $load                       = true,
-  Boolean $swap                       = true,
-  Boolean $procs                      = true,
-  Boolean $ntp                        = true,
-  Boolean $partitions                 = true,
-  Boolean $linux_raid                 = true,
-  Boolean $drbd                       = true,
-  Boolean $sensors                    = true,
-  Boolean $ide_smart                  = true,
-  Boolean $updates                    = true,
-  Boolean $ssh                        = true,
+  Boolean $load                          = true,
+  Boolean $swap                          = true,
+  Boolean $procs                         = true,
+  Boolean $ntp                           = true,
+  Boolean $partitions                    = true,
+  Boolean $linux_raid                    = true,
+  Boolean $drbd                          = true,
+  Boolean $sensors                       = true,
+  Boolean $ide_smart                     = true,
+  Boolean $updates                       = true,
+  Boolean $ssh                           = true,
   ## end of linux defaultchecks
   ## windows
-  Boolean $win_cpu                    = true,
-  Boolean $win_memory                 = true,
-  Boolean $win_network                = true,
-  Boolean $win_diskspace              = true,
-  Boolean $win_eventlog               = true,
-  Array[String] $win_eventlog_files   = ['system','application'],
+  Boolean $win_cpu                       = true,
+  Boolean $win_memory                    = true,
+  Boolean $win_network                   = true,
+  Boolean $win_diskspace                 = true,
+  Boolean $win_eventlog                  = true,
+  Array[String] $win_eventlog_files      = ['system','application'],
   ## end of windows defaultchecks
   ## common checks
-  Boolean $smtp                       = false,
-  Boolean $imap                       = false,
-  Boolean $pop                        = false,
-  Boolean $http                       = false,
-  Boolean $bacula                     = false,
-  Optional[String] $bacula_pass       = undef,
+  Boolean $smtp                          = false,
+  Boolean $imap                          = false,
+  Boolean $pop                           = false,
+  Boolean $http                          = false,
+  Boolean $bacula                        = false,
+  Optional[String] $bacula_pass          = undef,
   ## end of common checks
   ## specific linux checks
-  Boolean $mysql_local                = false,
-  Boolean $mysql_remote               = false,
-  Boolean $mysql_repl                 = false,
-  String $mysql_user                  = 'nagios',
-  Optional[String] $mysql_pass        = undef,
-  Boolean $pgsql_local                = false,
-  Boolean $pgsql_remote               = false,
-  String $pgsql_user                  = 'nagios',
-  Optional[String] $pgsql_pass        = undef,
-  Boolean $mongo_local                = false,
-  Boolean $mongo_remote               = false,
+  Boolean $mysql_local                   = false,
+  Boolean $mysql_remote                  = false,
+  Boolean $mysql_repl                    = false,
+  String $mysql_user                     = 'nagios',
+  Optional[String] $mysql_pass           = undef,
+  Boolean $pgsql_local                   = false,
+  Boolean $pgsql_remote                  = false,
+  String $pgsql_user                     = 'nagios',
+  Optional[String] $pgsql_pass           = undef,
+  Boolean $mongo_local                   = false,
+  Boolean $mongo_remote                  = false,
   ## end of specific linux checks
   ## specific windows checks
-  Array $win_services                 = [],
-  Boolean $mssql_remote               = false,
-  String $mssql_user                  = 'nagios',
-  Optional[String] $mssql_pass        = undef,
+  Array $win_services                    = [],
+  Boolean $mssql_remote                  = false,
+  String $mssql_user                     = 'nagios',
+  Optional[String] $mssql_pass           = undef,
 ) inherits ::nagios::params {
 
   tag $servers
@@ -158,9 +160,7 @@ class nagios::client(
     contacts => $host_contacts,
   }
 
-  if $contacts {
-    create_resources(::nagios::admin, $contacts)
-  }
+  create_resources(::nagios::admin, $contacts)
 
   if $ensure == 'present' {
     include "::nagios::client::checks::${::kernel}"
