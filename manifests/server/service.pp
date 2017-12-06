45bef305c399837439ddbaf9d706e9cc75442150
pajan la <pajan@yandex.ru>
2017-12-06 18:55:39 +0300

    small formatting

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -4,7 +4,7 @@ class nagios::server::service {
   assert_private('This is private class')
 
   service { $nagios::params::service_name:
-    ensure => $nagios::server::ensure ? { 'present' => 'running', 'absent' => undef },
-    enable => $nagios::server::ensure ? { 'present' => true, 'absent'      => undef },
+    ensure  => $nagios::server::ensure ? { 'present' => 'running', 'absent' => undef },
+    enable  => $nagios::server::ensure ? { 'present' => true, 'absent'      => undef },
   }
 }
