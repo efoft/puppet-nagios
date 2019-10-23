643846585187609da28c3a1185e70ff05f9f23bf
efoft <pajan@yandex.ru>
2019-10-23 23:11:54 +0300

    ensure_resource instead of ensure_resources for nagios_command

--- manifests/command.pp
+++ manifests/command.pp
@@ -4,5 +4,5 @@ define nagios::command (
   String[1] $command_line,
 ) {
 
-  ensure_resources('nagios_command', { $command_title => { 'command_line' => "\$USER1$/${command_line}" } })
+  ensure_resource('nagios_command', $command_title, { 'command_line' => "\$USER1$/${command_line}" })
 }
