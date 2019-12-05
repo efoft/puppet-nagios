55070a93269ebdcdb8af27d4f76550a95d8599c0
efoft <pajan@yandex.ru>
2019-12-05 22:08:30 +0300

    removed custom selinux module since Bug 1426824 resolved

--- manifests/server/selinux.pp
+++ manifests/server/selinux.pp
@@ -8,18 +8,4 @@ class nagios::server::selinux inherits nagios::server::install {
   }
 
   ensure_resource('selboolean','daemons_enable_cluster_mode', {'value' => 'on', 'persistent' => true})
-
-  if $ensure == 'present' {
-    if $::operatingsystemmajrelease == '7' {
-      # Bug 1426824 - Current selinux policy break nagios
-      selinux::audit2allow { 'nagios_script_to_pool':
-        avc_file => 'puppet:///modules/nagios/avc_nagios_script_to_pool.txt',
-      }
-    }
-    elsif $::operatingsystemmajrelease == '6' {
-      selinux::audit2allow { 'nagios_to_syslog':
-        avc_file => 'puppet:///modules/nagios/avc_nagios_to_syslog.txt',
-      }
-    }
-  }
 }
